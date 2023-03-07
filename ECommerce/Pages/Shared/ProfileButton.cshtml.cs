8dc62dc64ea9ec1724313c72cad5f0a3ec9f3f72
jburgos115 <burgosjonah@gmail.com>
2023-03-06 20:57:53 -0500

    added pages

---
+++ ECommerce/Pages/Shared/ProfileButton.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ECommerce.Pages.Shared
+{
+    public class ProfileButtonModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
