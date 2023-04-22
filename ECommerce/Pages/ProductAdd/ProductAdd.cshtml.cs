3b789bc88fae765278fa7d5c4f46663fd951a781
GavinShimp <gavinshimp@gmail.com>
2023-04-22 00:02:41 -0400

    shop management update

--- ECommerce/Pages/ProductAdd/ProductAdd.cshtml.cs
+++ ECommerce/Pages/ProductAdd/ProductAdd.cshtml.cs
@@ -16,31 +16,38 @@ namespace ECommerce.Pages.ProductAdd
 
         public Model.Products Products { get; set; }
 
+        public Model.User User { get; set; }
+
         public ProductAddModel(ApplicationDbContext db)
         {
             _db = db;
         }
-        public void OnGet(int id)
+        public void OnGet(int shopID)
         {
-            Shop = _db.Shop.Find(id); //Automatically opens db connection and executes SQL queries
+            Shop = _db.Shop.Find(shopID); //Automatically opens db connection and executes SQL queries
         }
 
         public async Task<IActionResult> OnPost(Model.Products products)
         {
-            //products.ShopID = shop.ShopID;
+            int x = 0;
+            string shopTemp = (string)Request.Query["shopID"];
+            Int32.TryParse(shopTemp, out x);
+
+            products.ShopID = x;
 
             try
             {
                 await _db.Products.AddAsync(products);
                 await _db.SaveChangesAsync();
                 TempData["success"] = "New Paper Created Successfully";
-                return RedirectToPage("Upload", "PaperID", new { ProductID = products.ProductID });
+                return RedirectToPage("../Upload/Upload", "ProductID", new { ProductID = products.ProductID });
             }
             catch (Exception ex)
             {
                 TempData["error"] = "Sorry, we are unable to process your request at this time. Please try again later.";
             }
-            return RedirectToPage("Index");
+            return RedirectToPage("../Index");
+
         }
     }
 }
