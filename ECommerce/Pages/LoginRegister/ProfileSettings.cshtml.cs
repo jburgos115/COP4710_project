2cb9077c8a5474e25c16e71e6b718fb5861689bc
jburgos115 <burgosjonah@gmail.com>
2023-04-22 00:02:40 -0400

    basic profile edit

--- ECommerce/Pages/LoginRegister/ProfileSettings.cshtml.cs
+++ ECommerce/Pages/LoginRegister/ProfileSettings.cshtml.cs
@@ -12,34 +12,95 @@ using ECommerce.Model;
 
 namespace ECommerce.Pages.LoginRegister
 {
+    [BindProperties]
     public class ProfileSettingsModel : PageModel
     {
-        /*private readonly ApplicationDbContext _db;
+        private readonly ApplicationDbContext _db;
+        private readonly IConfiguration _configuration;
+        private string connectionString;
         public User general { get; set; }
-        public ProfileSettingsModel(ApplicationDbContext db)
+        public ProfileSettingsModel(ApplicationDbContext db, IConfiguration configuration)
         {
             _db = db;
+            _configuration = configuration;
+            connectionString = _configuration["ConnectionStrings:DefaultConnection"];
+            general = new User();
         }
         public void OnGet()
         {
-            if (User.HasClaim("User", "General"))
+            try
             {
-                general = _db.User.Find(Convert.ToInt32(User.FindFirst("UserId").Value));
+                //Read appsettings for connection string
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    int id = Int32.Parse(User.FindFirst("UserID").Value);
+
+                    String myCommand = "SELECT Name, Email FROM [User] WHERE UserID = @UserID";
+
+                    SqlCommand cmd = new SqlCommand(myCommand, connection);
+                    cmd.Parameters.Add("@UserID", SqlDbType.Int).Value = id;
+
+                    using (SqlDataReader dataReader = cmd.ExecuteReader())
+                    {
+                        dataReader.Read();
+                        general.Name = dataReader["Name"].ToString();
+                        general.Email = dataReader["Email"].ToString();
+                    }
+
+                    cmd.Dispose();
+                    connection.Close();
+                }
             }
-            else if (User.HasClaim("UserCrewmember", "Crewmember"))
+            catch (Exception ex)
             {
-                crewmember = _db.Crewmember.Find(Convert.ToInt32(User.FindFirst("UserId").Value));
-            }
-            else if (User.HasClaim("manager", "Manager"))
-            {
-                manager = _db.Manager.Find(Convert.ToInt32(User.FindFirst("UserId").Value));
-            }
-            else
-            {
-                return new RedirectToPageResult("/LoginRegister/Login");
+                TempData["error"] = "Sorry, we are unable to process your request at this time. Please try again later.";
+                Response.Redirect("/../Index");
             }
 
-            return Page();
-        }*/
+            //return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            try
+            {
+                //Read appsettings for connection string
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    int id = Int32.Parse(User.FindFirst("UserID").Value);
+
+                    //Update User details in User table
+                    String myCommand = "UPDATE [User] SET Name = @Name, Email = @Email WHERE UserID = @UserID";
+
+                    SqlCommand cmd = new SqlCommand(myCommand, connection);
+                    cmd = new SqlCommand(myCommand, connection);
+
+                    cmd.Parameters.Add("@Name", SqlDbType.NVarChar, 50).Value = general.Name;
+                    cmd.Parameters.Add("@Email", SqlDbType.NVarChar, 50).Value = general.Email;
+                    cmd.Parameters.Add("@UserID", SqlDbType.Int).Value = id;
+
+                    int success = Convert.ToInt32(cmd.ExecuteNonQuery());
+
+                    //Check if query was unsuccessful
+                    if (success < 1)
+                    {
+                        TempData["error"] = "There was an error updating your profile, please try again later.";
+                    }
+
+                    cmd.Dispose();
+                    connection.Close();
+
+                    TempData["success"] = "Profile Updated Successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = "Sorry, we are unable to process your request at this time. Please try again later.";
+            }
+
+            return Redirect("/../Index");
+        }
     }
 }
